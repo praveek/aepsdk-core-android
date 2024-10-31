32b8837336cd963e79d917348cf34f0dabb6ecb2
Praveen <praveek@users.noreply.github.com>
2024-10-31 00:32:20 -0700

    Update build.gradle.kts

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.github.praveek:aepsdk-commons:e0479681c5")
+        classpath("com.github.praveek:aepsdk-commons:66c498cc3d")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
         classpath("androidx.benchmark:benchmark-gradle-plugin:1.2.3")
     }
